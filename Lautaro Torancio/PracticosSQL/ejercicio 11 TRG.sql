f2f41fe079c780aec549f201d5b07a4e09d94f2c
lautora05 <lautarot078@gmail.com>
2026-05-29 19:22:28 -0300

    Delete Lautaro Torancio/PracticosSQL/ejercicio 11 TRG.sql

--- Lautaro Torancio/PracticosSQL/ejercicio 11 TRG.sql
+++
@@ -1,9 +0,0 @@
-USE nuevo;
--- ejercicio 11 Enunciado: Registrar un pedido con multiples pasos. Usar SAVEPOINTs para poder deshacer pasos individuales.
-START TRANSACTION;
-INSERT INTO pedidos (estado, total) VALUES ('pendiente', 3000);
-SAVEPOINT sp_pedidos;
-INSERT INTO cuentas (id_cliente, tipo, saldo)
-VALUES (1, 'ahorro', 2500);
-SAVEPOINT sp_cuentas;
-COMMIT;
